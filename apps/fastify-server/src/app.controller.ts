4de7e417d27969b996efa8549ec2bb3edcf62d62
Tia <tia.zvajker@student.um.si>
2023-05-17 14:28:59 +0200

    popravki

--- apps/fastify-server/src/app.controller.ts
+++ apps/fastify-server/src/app.controller.ts
@@ -1,8 +1,5 @@
 import { Controller, Get, Post, Req, Res, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import fastify = require('fastify');
-import { ApiFileBody, MulterFile } from '@webundsoehne/nest-fastify-file-upload';
 
 @Controller('files')
 export class AppController {
@@ -11,28 +8,6 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-/*
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  @ApiFileBody('file')
-  @ApiConsumes('multipart/form-data')
-  uploadFile (@UploadedFile('file') file: MulterFile): void {
-      console.log(file)
-  }
-*/
-/*
-  @Post('/uploadFile')
-  async uploadFile(@Req() req: fastify.FastifyRequest, @Res() res: fastify.FastifyReply<any>): Promise<any> {
-    return await this.appService.uploadFile(req,res)
-  }  
-  */
-  @Post('upload')
-  @UseInterceptors(
-    FilesInterceptor('image')
-  )
-  uploadFiles(@UploadedFiles() file: Express.Multer.File){
-    console.log(file);
   }
 
 }
