1f5b7b2237e5ea271033deb5dc44dd334f744927
Elena <elena.ugwoegbulam@student.um.si>
2023-10-24 19:34:31 +0200

    product mapper done

--- apps/fastify-server/src/app/products/product.mapper.spec.ts
+++ apps/fastify-server/src/app/products/product.mapper.spec.ts
@@ -2,36 +2,32 @@ import { Mock } from 'ts-mockery';
 import { Product } from './product.model';
 import { ProductCategory } from './product-category.enum';
 import { ProductMapper } from './product.mapper';
+import { sampleSeller } from '../sellers/seller.mock';
+import { ProductResponse } from 'src/data.response';
 
 const sampleProduct = Mock.of<Product>({
+    id: "1",
     title: 'Sample Product',
     drinkCategory: ProductCategory.BEER,
     packaging: 'Bottle',
     size: '500ml',
     price: 2.99,
     stock: 100,
-    seller: {
-      name: 'Sample Seller',
-      rating: 4.5,
-      // itd.
-    },
+    seller: sampleSeller,
     actualPrice: 2.99,
     discount: 0,
     picture: 'sample-product.jpg',
   });
 
-  const responseProduct = Mock.of<Product>({
+  const responseProduct = Mock.of<ProductResponse>({
+    id: "1",
     title: 'Sample Product',
     drinkCategory: ProductCategory.BEER,
     packaging: 'Bottle',
     size: '500ml',
     price: 2.99,
     stock: 100,
-    seller: {
-      name: 'Sample Seller',
-      rating: 4.5,
-      // itd.
-    },
+    seller: sampleSeller,
     actualPrice: 2.99,
     discount: 0,
     picture: 'sample-product.jpg',
@@ -45,7 +41,7 @@ const sampleProduct = Mock.of<Product>({
       productMapper = new ProductMapper();
     });
   
-    it('should map a Buyer to a BuyerResponse', () => {
+    it('should map a Product to a ProductResponse', () => {
       const response = productMapper.mapToResponse(sampleProduct);
       expect(response).toStrictEqual(responseProduct);
     });
